3e8035dfcad1ecb6c74495bb32951b78630e0b27
Christian Ruigrok <christian@ruigrok.info>
2017-09-15 19:37:18 +0200

    Add uglify to webpack and fix build

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 const nodeExternals = require('webpack-node-externals');
 const NodemonPlugin = require('nodemon-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 const debug = process.env.NODE_ENV === 'development';
 
@@ -9,7 +10,7 @@ const config = {
   target: 'node',
   entry: path.resolve(__dirname, 'src/index.ts'),
   output: {
-    filename: 'index.js',
+    filename: 'index-[hash].js',
     path: path.resolve(__dirname, 'dist'),
     hotUpdateChunkFilename: 'hot/hot-update.js',
     hotUpdateMainFilename: 'hot/hot-update.json'
@@ -18,14 +19,17 @@ const config = {
     rules: [
       {
         test: /\.jsx?$/,
+        include: path.resolve(__dirname, 'src'),
         use: ['babel-loader', 'eslint-loader']
       },
       {
         test: /\.tsx?$/,
+        include: path.resolve(__dirname, 'src'),
         use: ['babel-loader', 'ts-loader', 'eslint-loader']
       },
       {
         test: /\.json$/,
+        include: path.resolve(__dirname, 'src'),
         use: 'json-loader'
       }
     ]
@@ -41,7 +45,16 @@ const config = {
       entryOnly: false
     })
   ],
+  devtool: 'source-map',
   stats: {
+    colors: true
+  }
+};
+
+if (debug) {
+  config.output.filename = 'index.js';
+  config.devtool = '#eval-source-map';
+  config.stats = {
     assets: false,
     colors: true,
     version: false,
@@ -49,12 +62,25 @@ const config = {
     timings: false,
     chunks: false,
     chunkModules: false
-  }
-};
-
-if (debug) {
-  config.devtool = '#eval-source-map';
+  };
   config.plugins.push(new NodemonPlugin());
+} else {
+  config.plugins.push(
+    new UglifyJSPlugin({
+      sourceMap: true,
+      parallel: true,
+      uglifyOptions: {
+        ecma: 8
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false,
+      options: {
+        context: __dirname
+      }
+    })
+  );
 }
 
 module.exports = config;
